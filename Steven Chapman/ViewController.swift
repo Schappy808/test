c4256008ffe4669351ba33405d10d934c9a23a2d
Steven Chapman <steven44@hawaii.edu>
2020-01-16 10:08:25 -1000

    Hackwich1-2

---
+++ Steven Chapman/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Steven Chapman
+//
+//  Created by CM Student on 1/16/20.
+//  Copyright © 2020 Steven Chapman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Steven ChapmanTests/Steven_ChapmanTests.swift
@@ -0,0 +1,34 @@
+//
+//  Steven_ChapmanTests.swift
+//  Steven ChapmanTests
+//
+//  Created by CM Student on 1/16/20.
+//  Copyright © 2020 Steven Chapman. All rights reserved.
+//
+
+import XCTest
+@testable import Steven_Chapman
+
+class Steven_ChapmanTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
